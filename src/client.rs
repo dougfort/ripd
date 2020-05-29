c1a345876e5ba51e4f509af748a67bdfc0e09fcf
dougfort <doug.fort@gmail.com>
2020-05-29 18:42:01 -0400

    basic shell of a non-streaming game

--- src/client.rs
+++ src/client.rs
@@ -1,61 +1,42 @@
 #[macro_use]
 extern crate log;
 
-use std::io::Write;
-use std::io::{stdin, stdout};
-
 use ipd::ipd_client::IpdClient;
-use ipd::{NewGameRequest};
+use ipd::{ActionRequest, NewGameRequest};
 
 mod ipd;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut player_name: String = "".to_string();
+    env_logger::init();
+    let player_name: String = "noname".to_string();
+    let addr = "http://[::1]:50051";
 
-    let channel = tonic::transport::Channel::from_static("http://[::1]:50051")
+    info!("connecting to {}", addr);
+    let channel = tonic::transport::Channel::from_static(addr)
         .connect()
         .await?;
     let mut client = IpdClient::new(channel);
-    loop {
-        let stdin_line = get_stdin_line(&format!("({}) >", player_name))?;
-        let command: Vec<&str> = stdin_line.trim().split_whitespace().collect();
-        if command.is_empty() {
-            continue;
-        }
-        match command[0].trim() {
-            "" => continue,
-            "quit" => break,
-            "new" => {
-                if command.len() < 2 {
-                    println!("you must specify a player name");
-                    continue;
-                }
-                player_name = command[1].trim().to_owned();
-                info!("requesting new game: {}", player_name);
-                let request = tonic::Request::new(NewGameRequest {
-                    player_name: player_name.to_string(),
-                });
-                info!("waiting response");
-                let response = client.new_game(request).await?.into_inner();
-                info!(
-                    "game is = {}; opponenent = {}",
-                    response.game_id, response.opponent_name
-                );
-            }
-            _ => println!("unknown command: '{:?}'", command),
-        }
-    }
+    info!("requesting new game: {}", player_name);
+    let game_request = tonic::Request::new(NewGameRequest {
+        player_name: player_name.to_string(),
+    });
+    info!("waiting response");
+    let game_response = client.new_game(game_request).await?.into_inner();
+    info!(
+        "game is = {}; opponenent = {}",
+        game_response.game_id, game_response.opponent_name
+    );
+    let action_request = tonic::Request::new(ActionRequest {
+        game_id: game_response.game_id,
+        sequence: 1,
+        action: 1,
+    });
+    let action_result = client.play(action_request).await?.into_inner();
+    info!(
+        "game: {}, sequence: {}, action: {}, score: {}",
+        action_result.game_id, action_result.sequence, action_result.action, action_result.score
+    );
+
     Ok(())
-}
-
-fn get_stdin_line(prompt: &str) -> std::io::Result<String> {
-    println!();
-    print!("{} ", prompt);
-    stdout().flush()?;
-
-    let mut input = String::new();
-    stdin().read_line(&mut input)?;
-
-    Ok(input)
 }
--- src/server.rs
+++ src/server.rs
@@ -3,16 +3,32 @@ extern crate log;
 
 use ipd::ipd_server::{Ipd, IpdServer};
 use ipd::{ActionRequest, ActionResult, NewGameRequest, NewGameResponse};
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicU32, Ordering};
+use std::sync::Mutex;
 use tokio::sync::mpsc;
 use tonic::{transport::Server, Request, Response, Status, Streaming};
 mod ipd;
 
 #[derive(Default)]
-pub struct IpdData {}
+pub struct GameData {
+    sequence: u64,
+    players: (String, String),
+    history: Vec<(i32, i32)>,
+}
+
+#[derive(Default)]
+pub struct IpdData {
+    game_id: AtomicU32,
+    game_map: Mutex<HashMap<u32, GameData>>,
+}
 
 impl IpdData {
     fn new() -> Self {
-        IpdData {}
+        IpdData {
+            game_id: AtomicU32::new(1),
+            game_map: Mutex::new(HashMap::new()),
+        }
     }
 }
 
@@ -25,10 +41,26 @@ impl Ipd for IpdData {
         &self,
         request: Request<NewGameRequest>,
     ) -> Result<Response<NewGameResponse>, Status> {
-        info!("new_game: {}", request.get_ref().player_name);
+        let request = request.into_inner();
+        let game_id = self.game_id.fetch_add(1, Ordering::Relaxed);
+
+        info!(
+            "new_game: player: {}; game_id: {}",
+            request.player_name, game_id
+        );
+
+        let game_data = GameData {
+            sequence: 1,
+            players: ("Calculon".to_string(), request.player_name),
+            history: Vec::new(),
+        };
+        {
+            let mut game_map = self.game_map.lock().unwrap();
+            game_map.insert(game_id, game_data);
+        }
         Ok(Response::new(NewGameResponse {
-            game_id: "foot".to_string(),
-            opponent_name: "clam".to_string(),
+            game_id,
+            opponent_name: "Calculon".to_string(),
         }))
     }
 
@@ -36,8 +68,27 @@ impl Ipd for IpdData {
         &self,
         request: Request<ActionRequest>,
     ) -> Result<Response<ActionResult>, Status> {
-        debug!("play: {:?}", request);
-        Err(Status::unimplemented("play"))
+        let request = request.into_inner();
+        let game_id = request.game_id;
+        let opponent_action = request.action;
+        let mut game_map = self.game_map.lock().unwrap();
+        let game_data = match game_map.get_mut(&game_id) {
+            Some(gd) => gd,
+            None => return Err(Status::unavailable(format!("no such game: {}", game_id))),
+        };
+        game_data.sequence += 1;
+        let our_action = 1;
+        info!(
+            "play: game_id: {}; sequence: {}; action: ({:?}, {:?})",
+            game_id, game_data.sequence, opponent_action, our_action
+        );
+        game_data.history.push((opponent_action, our_action));
+        Ok(Response::new(ActionResult {
+            game_id,
+            sequence: game_data.sequence,
+            action: our_action,
+            score: 0,
+        }))
     }
 
     async fn play_game(
